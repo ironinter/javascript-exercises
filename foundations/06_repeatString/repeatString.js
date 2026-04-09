792fbb7bbf15b8f4be8475cb5ee9c17e6b080e27
ironinter <anonymousk539@gmail.com>
2026-04-10 00:56:17 +0530

    Complete 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
 
+    let strRepeat = '';
+    for (let i = 0; i < num; i++) {
+        strRepeat += str;
+    }
+    return strRepeat;
 };
 
 // Do not edit below this line
