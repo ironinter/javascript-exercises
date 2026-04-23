993338b4779aebd31e70d071b6ed7cb123a39c74
ironinter <anonymousk539@gmail.com>
2026-04-24 04:08:10 +0530

    Correct 10 (with a single if statement)

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,10 +1,13 @@
 const leapYears = function(year) {
     // No tests for confirming year's type (non-negative int / NaN / NULL / infinity / 'string' / etc)
-    let counter = false;    
-    if (year % 4 === 0) counter = true;
-    if (year % 100 === 0) counter = false; 
-    if (year % 400 === 0) counter = true;
-    return counter;
+    // let counter = false;    
+    // if (year % 4 === 0) counter = true;
+    // if (year % 100 === 0) counter = false; 
+    // if (year % 400 === 0) counter = true;
+    // return counter;
+
+    if ((year%400===0) || ((year%4===0) && (year%100!==0))) return true;
+    return false;
 };
 
 
