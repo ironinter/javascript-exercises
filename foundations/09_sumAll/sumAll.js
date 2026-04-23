418a0b9c4817a791f40fa361f6e00862be5033b0
ironinter <anonymousk539@gmail.com>
2026-04-24 02:11:24 +0530

    Complete 09.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let a = 0, b = 0;
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+    else if (num1 < 0 || num2 < 0) return 'ERROR';
+
+
+    if (num1 > num2) {
+        a = num2;
+        b = num1;
+    } else if (num1 < num2) {
+        a = num1;
+        b = num2;
+    } 
+
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
