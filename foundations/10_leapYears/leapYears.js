e98c1786173e8b39b79a5371277121c02b1f4945
ironinter <anonymousk539@gmail.com>
2026-04-24 03:35:47 +0530

    Complete 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // No tests for confirming year's type (non-negative int / NaN / NULL / infinity / 'string' / etc)
+    let counter = false;    
+    if (year % 4 === 0) counter = true;
+    if (year % 100 === 0) counter = false; 
+    if (year % 400 === 0) counter = true;
+    return counter;
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
