ab7822331c514884f2d344d47bacd01b2aa2e8ff
ironinter <anonymousk539@gmail.com>
2026-04-23 20:41:52 +0530

    Complete 08.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function(arr, item) {
+//     for (let i = 1; i < arguments.length; i++) {
+
+//         arr.forEach((element, index) => {
+//             if (element === arguments[i]) arr.splice(index, 1);
+//         });
+//     }
+//     return arr;  
+// };
+
+const removeFromArray = function(arr, ...toRemoves) {
+    let arr2 = [];
+    for (element of arr) {
+        if (!toRemoves.includes(element)) arr2.push(element); 
+    }
+    return arr2;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
